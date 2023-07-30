9d49ce7031f9f2e3347f95dc9342ec718dc91e1c
Rakshitha S <62503907+rakshithamatthuga@users.noreply.github.com>
2023-07-30 17:30:33 +0530

    'addedserverdetails'

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const app=express();
+const chors=require('chors')
+app.use(express.json());
 const products=require('./db/products')
 const person=[{item:'Pants',price:24},{item:'Shoes',price:25}];
 app.get("/api/products",(request, response)=>{
@@ -7,6 +9,10 @@ app.get("/api/products",(request, response)=>{
 })
 app.get("/cart",(request, response)=>{
   response.send('Hello from Express Server')
+})
+
+app.post("/api/auth/login",(req,res)=>{
+    console.log(req);
 })
 app.listen(3000,() =>{
   console.log('server is up and running')
---
+++ users.js
@@ -0,0 +1,26 @@
+const {v4:uuid}=require('uuid')
+const users={
+    data:[
+        {
+            id:uuid(),
+            email:'raksh@gmail.com',
+            password:"12345",
+            number:'9876543211',
+            name:'Rakshitha'
+        },
+        {
+            id:uuid(),
+            email:'ash@gmail.com',
+            password:"12345",
+            number:'9876543212',
+            name:'Ashritha'
+        },
+        {
+            id:uuid(),
+            email:'rekha@gmail.com',
+            password:"12345",
+            number:'9876543311',
+            name:'Rekha'
+        }
+    ]
+}
